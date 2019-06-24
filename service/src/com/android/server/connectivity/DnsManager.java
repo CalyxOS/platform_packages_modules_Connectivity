ff712c4cba9f9d6d4674f40721c08154e4a9178a
Chirayu Desai <chirayudesai1@gmail.com>
2019-06-24 21:27:35 +0530

    Add Cloudflare DNS as a private DNS provider
    TODO: Add tests
    
    Change-Id: I7e8a320d47e7c5ddbcb9acfaf23032ae92d5d70d
    (cherry-picked from commit 96ce93f7bf7233008591d22a0d24a6d7cd0a94ff)
    
    Change-Id: I98f4f832f0a1f290487a2d0ebf1567062d83920e

--- service/src/com/android/server/connectivity/DnsManager.java
+++ service/src/com/android/server/connectivity/DnsManager.java
@@ -23,8 +23,12 @@ import static android.net.ConnectivitySettingsManager.DNS_RESOLVER_SUCCESS_THRES
 import static android.net.ConnectivitySettingsManager.PRIVATE_DNS_DEFAULT_MODE;
 import static android.net.ConnectivitySettingsManager.PRIVATE_DNS_MODE;
 import static android.net.ConnectivitySettingsManager.PRIVATE_DNS_MODE_OFF;
+import static android.net.ConnectivitySettingsManager.PRIVATE_DNS_MODE_CLOUDFLARE;
 import static android.net.ConnectivitySettingsManager.PRIVATE_DNS_MODE_PROVIDER_HOSTNAME;
 import static android.net.ConnectivitySettingsManager.PRIVATE_DNS_SPECIFIER;
+import static android.net.ConnectivitySettingsManager.PRIVATE_DNS_SPECIFIER_CLOUDFLARE;
+import static android.net.ConnectivitySettingsManager.DNS_CLOUDFLARE_IP1;
+import static android.net.ConnectivitySettingsManager.DNS_CLOUDFLARE_IP2;
 import static android.net.resolv.aidl.IDnsResolverUnsolicitedEventListener.VALIDATION_RESULT_FAILURE;
 import static android.net.resolv.aidl.IDnsResolverUnsolicitedEventListener.VALIDATION_RESULT_SUCCESS;
 
@@ -139,6 +143,13 @@ public class DnsManager {
             final String specifier = getStringSetting(context.getContentResolver(),
                     PRIVATE_DNS_SPECIFIER);
             return new PrivateDnsConfig(specifier, null);
+        }
+
+        if (PRIVATE_DNS_MODE_CLOUDFLARE.equals(mode)) {
+            InetAddress[] ips = new InetAddress[]{
+                    InetAddress.parseNumericAddress(DNS_CLOUDFLARE_IP1),
+                    InetAddress.parseNumericAddress(DNS_CLOUDFLARE_IP2)};
+            return new PrivateDnsConfig(PRIVATE_DNS_SPECIFIER_CLOUDFLARE, ips);
         }
 
         return new PrivateDnsConfig(useTls);
