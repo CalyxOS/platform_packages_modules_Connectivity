1ce8394b096feb0d89e8becac207e6f63d74f631
Oliver Scott <olivercscott@gmail.com>
2021-07-07 08:54:08 -0400

    Disallow non-VPN tethering upstreams on always-on VPN lockdown mode
    * This ensures that the Hotspot "Allow clients to use VPN" option
      respects the VPN "Block connections without VPN" option,
      to avoid any leaks.
    
    Issue: calyxos#555
    Change-Id: Ibd690c605a3d87e3c493550039a65862be16848b

--- Tethering/src/com/android/networkstack/tethering/UpstreamNetworkMonitor.java
+++ Tethering/src/com/android/networkstack/tethering/UpstreamNetworkMonitor.java
@@ -39,6 +39,7 @@ import android.net.NetworkRequest;
 import android.net.util.PrefixUtils;
 import android.net.util.SharedLog;
 import android.os.Handler;
+import android.provider.Settings;
 import android.util.Log;
 import android.util.SparseIntArray;
 
@@ -87,6 +88,9 @@ public class UpstreamNetworkMonitor {
     private static final String TAG = UpstreamNetworkMonitor.class.getSimpleName();
     private static final boolean DBG = false;
     private static final boolean VDBG = false;
+
+    // Copied from core/java/android/provider/Settings.java
+    private static final String ALWAYS_ON_VPN_LOCKDOWN = "always_on_vpn_lockdown";
 
     public static final int EVENT_ON_CAPABILITIES   = 1;
     public static final int EVENT_ON_LINKPROPERTIES = 2;
@@ -334,10 +338,14 @@ public class UpstreamNetworkMonitor {
      */
     public UpstreamNetworkState getCurrentPreferredUpstream() {
         // Use VPN upstreams if hotspot settings allow.
-        if (mVpnInternetNetwork != null &&
-                LineageSettings.Secure.getInt(mContext.getContentResolver(),
+        if (LineageSettings.Secure.getInt(mContext.getContentResolver(),
                         LineageSettings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS, 0) == 1) {
-            return mNetworkMap.get(mVpnInternetNetwork);
+            if (mVpnInternetNetwork != null) {
+                return mNetworkMap.get(mVpnInternetNetwork);
+            } else if (Settings.Secure.getInt(mContext.getContentResolver(),
+                    ALWAYS_ON_VPN_LOCKDOWN, 0) == 1) {
+                return null;
+            }
         }
         final UpstreamNetworkState dfltState = (mDefaultInternetNetwork != null)
                 ? mNetworkMap.get(mDefaultInternetNetwork)
