c52394f8304daaba482412032b1d5664eb3fe6ce
Oliver Scott <olivercscott@gmail.com>
2022-01-21 13:57:33 +0100

    Expose getActiveNetworkForUid to system API
    * When checking if a UID has network access in restricted networking mode, it is best to query the active network for that UID
    * Updated module-lib-current.txt with "m framework-connectivity.stubs.source.module_lib-update-current-api"
    
    Change-Id: I75f952e29b9069aab138e6d77c1c7be659180331

--- framework/src/android/net/ConnectivityManager.java
+++ framework/src/android/net/ConnectivityManager.java
@@ -1197,6 +1197,9 @@ public class ConnectivityManager {
     }
 
     /** {@hide} */
+    @RequiresPermission(android.Manifest.permission.NETWORK_STACK)
+    @SystemApi(client = MODULE_LIBRARIES)
+    @Nullable
     public Network getActiveNetworkForUid(int uid, boolean ignoreBlocked) {
         try {
             return mService.getActiveNetworkForUid(uid, ignoreBlocked);
