401b620027bd6473861e6e0bbbf6de6c2e8ac586
t-m-w <tmw-likes-calyx@pm.me>
2022-09-20 17:12:48 +0000

    Use BPF to block socket creation when restricted (TESTING)
    Adapt BPF's inet socket creation rule to not only check INTERNET
    permission but also to consider whether the uid is denied in any of
    the firewall chains, including the restricted chain.
    
    Co-authored-by: Oliver Scott <olivercscott@gmail.com>
    Change-Id: I912a4a2ee78a29ca8b7d8ff85e5ad7cf617c31a5

--- bpf_progs/netd.c
+++ bpf_progs/netd.c
@@ -51,6 +51,11 @@
 #define TCP_FLAG_OFF 13
 #define RST_OFFSET 2
 
+#define IFINDEX_LOOPBACK 1
+#define IFINDEX_IGNORED IFINDEX_LOOPBACK
+#define FOR_CREATE true
+#define FOR_TRAFFIC false
+
 // For maps netd does not need to access
 #define DEFINE_BPF_MAP_NO_NETD(the_map, TYPE, TypeOfKey, TypeOfValue, num_entries) \
     DEFINE_BPF_MAP_EXT(the_map, TYPE, TypeOfKey, TypeOfValue, num_entries, \
@@ -217,10 +222,8 @@ static __always_inline BpfConfig getConfig(uint32_t configKey) {
     return *config;
 }
 
-static inline int bpf_owner_match(struct __sk_buff* skb, uint32_t uid, int direction) {
-    if (skip_owner_match(skb)) return BPF_PASS;
-
-    if (is_system_uid(uid)) return BPF_PASS;
+static __always_inline inline int bpf_owner_rule_match(bool forCreate, uint32_t uid, int ifindex) {
+    if (is_system_uid(forCreate ? (uid % AID_USER_OFFSET) : uid)) return BPF_PASS;
 
     BpfConfig enabledRules = getConfig(UID_RULES_CONFIGURATION_KEY);
 
@@ -254,9 +257,9 @@ static inline int bpf_owner_match(struct __sk_buff* skb, uint32_t uid, int direc
             return BPF_DROP;
         }
     }
-    if (direction == BPF_INGRESS && skb->ifindex != 1) {
+    if (ifindex != IFINDEX_LOOPBACK) {
         if (uidRules & IIF_MATCH) {
-            if (allowed_iif && skb->ifindex != allowed_iif) {
+            if (allowed_iif && ifindex != allowed_iif) {
                 // Drops packets not coming from lo nor the allowed interface
                 // allowed interface=0 is a wildcard and does not drop packets
                 return BPF_DROP_UNLESS_DNS;
@@ -268,6 +271,13 @@ static inline int bpf_owner_match(struct __sk_buff* skb, uint32_t uid, int direc
         }
     }
     return BPF_PASS;
+}
+
+static inline int bpf_owner_match(struct __sk_buff* skb, uint32_t uid, int direction) {
+    if (skip_owner_match(skb)) return BPF_PASS;
+
+    return bpf_owner_rule_match(FOR_TRAFFIC, uid,
+            direction == BPF_INGRESS ? skb->ifindex : IFINDEX_IGNORED);
 }
 
 static __always_inline inline void update_stats_with_config(struct __sk_buff* skb, int direction,
@@ -428,22 +438,24 @@ DEFINE_XTBPF_PROG("skfilter/denylist/xtbpf", AID_ROOT, AID_NET_ADMIN, xt_bpf_den
 DEFINE_BPF_PROG_EXT("cgroupsock/inet/create", AID_ROOT, AID_ROOT, inet_socket_create,
                     KVER(4, 14, 0), KVER_INF, false, "fs_bpf_netd_readonly", "")
 (struct bpf_sock* sk) {
-    uint64_t gid_uid = bpf_get_current_uid_gid();
+    uint32_t uid = (bpf_get_current_uid_gid() & 0xffffffff);
     /*
      * A given app is guaranteed to have the same app ID in all the profiles in
      * which it is installed, and install permission is granted to app for all
      * user at install time so we only check the appId part of a request uid at
      * run time. See UserHandle#isSameApp for detail.
      */
-    uint32_t appId = (gid_uid & 0xffffffff) % AID_USER_OFFSET;  // == PER_USER_RANGE == 100000
+    uint32_t appId = uid % AID_USER_OFFSET;  // == PER_USER_RANGE == 100000
     uint8_t* permissions = bpf_uid_permission_map_lookup_elem(&appId);
-    if (!permissions) {
-        // UID not in map. Default to just INTERNET permission.
-        return 1;
-    }
 
-    // A return value of 1 means allow, everything else means deny.
-    return (*permissions & BPF_PERMISSION_INTERNET) == BPF_PERMISSION_INTERNET;
+    // If UID is in map but missing BPF_PERMISSION_INTERNET, app has no INTERNET permission.
+    if (permissions && ((*permissions & BPF_PERMISSION_INTERNET) != BPF_PERMISSION_INTERNET)) {
+        return 0; // Deny
+    } else {
+        // Only allow if uid is not restricted.
+        // A return value of 1 means allow, everything else means deny.
+        return bpf_owner_rule_match(FOR_CREATE, uid, IFINDEX_IGNORED) == BPF_PASS ? 1 : 0;
+    }
 }
 
 LICENSE("Apache 2.0");
