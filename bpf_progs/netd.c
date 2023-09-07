44c5837b8e652bd38e647932370ea1339b4d6dc4
Tommy Webb <tommy@calyxinstitute.org>
2023-09-07 03:59:21 -0400

    Deny socket creation for transport-blocked apps
    Prevent apps whose network access is blocked based on transport
    policies from creating sockets.
    
    Squash with: I912a4a2ee78a29ca8b7d8ff85e5ad7cf617c31a5
    
    Change-Id: I995e9929f6f8c1ae0613e05e0cade55a76c35902

--- bpf_progs/netd.c
+++ bpf_progs/netd.c
@@ -218,16 +218,16 @@ static __always_inline BpfConfig getConfig(uint32_t configKey) {
 }
 
 // Must be __always_inline or the call from inet_socket_create will crash-reboot the system
-static __always_inline inline int bpf_owner_restricted_match(uint32_t uid) {
+static __always_inline inline int bpf_owner_firewall_match(uint32_t uid) {
     if (is_system_uid(uid)) return BPF_PASS;
 
     const BpfConfig enabledRules = getConfig(UID_RULES_CONFIGURATION_KEY);
 
-    if (enabledRules & RESTRICTED_MATCH) {
+    if (enabledRules & (RESTRICTED_MATCH | OEM_DENY_1_MATCH)) {
         const UidOwnerValue* uidEntry = bpf_uid_owner_map_lookup_elem(&uid);
         const uint32_t uidRules = uidEntry ? uidEntry->rule : 0;
 
-        if (!(uidRules & RESTRICTED_MATCH)) {
+        if (!(uidRules & (RESTRICTED_MATCH | OEM_DENY_1_MATCH))) {
             return BPF_DROP;
         }
     }
@@ -458,8 +458,8 @@ DEFINE_BPF_PROG_EXT("cgroupsock/inet/create", AID_ROOT, AID_ROOT, inet_socket_cr
     if (permissions && ((*permissions & BPF_PERMISSION_INTERNET) != BPF_PERMISSION_INTERNET)) {
         return BPF_DROP;
     }
-    // Only allow if uid is not restricted.
-    return bpf_owner_restricted_match(uid);
+    // Only allow if uid is not blocked by the user firewall.
+    return bpf_owner_firewall_match(uid);
 }
 
 LICENSE("Apache 2.0");
